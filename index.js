5a99a85ff9feef604e535bea5b1d4657a8cef629
asyven <asyven3@gmail.com>
2023-01-04 19:51:46 +0200

    u

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+app.post('/', (req, res) => {
+    try {
+        console.log(req.body)
+        return res.json().status(200);
+    } catch (e) {
+        console.log(e)
+    }
 })
+
 app.listen(process.env.PORT || 3000)
